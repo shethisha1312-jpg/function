eb2747946cbc810fb926edd6bf5976fab91ac09d
shethisha1312-jpg <shethisha1312@gmail.com>
2026-01-29 20:58:21 +0530

    function

---
+++ script.js
@@ -0,0 +1,67 @@
+// function myfuction(){
+//   console.log("ish sheth");
+//     console.log("charmy  ");
+// }
+// myfuction();
+
+// function myfuction(msg){
+//     //parameter -> input
+//     console.log(msg);
+// } 
+
+// myfuction("html css js",100);//argument
+
+//function -> 2 number , sum
+
+// function sum(x,y){
+//     s=x+y;     
+//     return s;
+// }
+
+// let  val = sum(3,4);
+// console.log(val);
+ 
+//sum fuction
+// function sum(a,b){
+//     return a+b;
+// }
+// const arrowsum = (a,b)=>{
+//     console.log(a+b);
+// };
+
+// //multiplication function
+// function mul(a,b){
+//     return a*b;
+
+// }
+// const arrowmul = (a,b)=>{
+//     console.log(a*b);
+// };
+
+// function countVowels(str){
+//     let count = 0;
+//     for (const char of str){
+//         if(char ==="a"||char ==="e"||char ==="i"||char ==="o"||char ==="u")
+//             count++;
+
+//     }
+//     return count;
+// }
+// const  countVow =(str)=>{
+//     let count = 0;
+//     for (const char of str){
+//         if(char ==="a"||char ==="e"||char ==="i"||char ==="o"||char ==="u")
+//         {   count++;
+//         } 
+//     }
+//     return count;
+
+// };
+
+//foreach loop in array
+let arr=["rajkot","mumbai","pune","barpda"];
+arr.forEach((val,idx,arr)=>{
+    console.log(val.toUpperCase(),idx,arr);
+});
+
+ 
